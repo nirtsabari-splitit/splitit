d3149af8941aafc28e055fd29a122a7e23291540
nir.tsabari <nirtsabari@outlook.com>
2024-01-08 22:49:33 +0200

    Add properties to Actor class

--- Actor.cs
+++ Actor.cs
@@ -1,1 +1,9 @@
-public class Actor { }
+public class Actor
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Details { get; set; }
+    public string Type { get; set; }
+    public int Rank { get; set; }
+    public string Source { get; set; }
+}
